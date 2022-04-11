89539887f9b0d753d277eaf27b5dd3f51b205c90
Rosilene Goncalves Ghensev Peres <rosilene.peres@livelo.com.br>
2022-04-11 11:29:19 -0300

    add while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,12 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 38
+total_tentativas = 3
+rodada = 1
 
-tentativa = int(input("Digite um número: "))
+while(rodada <= total_tentativas):
+    print("Tentativa:", rodada, "de", total_tentativas)
+    tentativa = int(input("Digite um número: "))
 
-print("Você digitou ", tentativa)
+    acertou = tentativa == numero_secreto
+    maior = tentativa > numero_secreto
+    menor = tentativa < numero_secreto
 
-if(tentativa == numero_secreto):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+    print("Você digitou ", tentativa)
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! Tente chutar um número menor.")
+        elif (menor):
+            print("Você errou! Tente chutar um número maior.")
+
+    rodada = rodada + 1
+
+print("Fim de jogo!")
