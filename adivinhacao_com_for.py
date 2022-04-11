7588cdd3e26876f787052285098e9116a0b868e7
Rosilene Goncalves Ghensev Peres <rosilene.peres@livelo.com.br>
2022-04-11 13:54:19 -0300

    add arquivo formatacao de strings

--- adivinhacao_com_for.py
+++ adivinhacao_com_for.py
@@ -9,6 +9,10 @@ for rodada in range(1, total_tentativas + 1):
     print("Tentativa: {} de {}".format(rodada, total_tentativas))
     tentativa = int(input("Digite um número entre 1 e 100: "))
 
+    if(tentativa < 1 or tentativa > 100):
+        print("Você deve digitar um número entre 1 e 100! ")
+        continue
+
     acertou = tentativa == numero_secreto
     maior = tentativa > numero_secreto
     menor = tentativa < numero_secreto
---
+++ formatacao_de_strings.py
@@ -0,0 +1,30 @@
+print("*********************************")
+print("Exemplos de Formatação de Strings")
+print("*********************************")
+
+print("Link da documentação: https://docs.python.org/3/library/string.html#formatexamples")
+print("*********************************")
+
+print("Invertendo a ordem:")
+print("Tentativa {1} de {0}".format(1, 3))
+
+print("Formatação de float:")
+print("R$ {:f}".format(1.59))
+print("Definindo 2 casas depois da vírgula:")
+print("R$ {:.2f}".format(1.59))
+print("R$ {:.2f}".format(1.5))
+print("R$ {:.2f}".format(1))
+
+print("Definindo quantidade de caracteres e 2 casas depois da vírgula:")
+print("R$ {:7.2f}".format(1234.50))
+print("R$ {:7.2f}".format(1.59))
+print("R$ {:7.2f}".format(1))
+
+print("Definindo quantidade de caracteres, colocando zeros à frente e 2 casas depois da vírgula:")
+print("R$ {:07.2f}".format(1234.50))
+print("R$ {:07.2f}".format(1.59))
+print("R$ {:07.2f}".format(1))
+
+print("Formatação de data:")
+print("Data {:02d}/{:02d}".format(9, 4))
+print("Data {:02d}/{:02d}".format(19, 11))
